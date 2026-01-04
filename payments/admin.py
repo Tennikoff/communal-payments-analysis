bb15c6557a85088a45d78578d5c60a7f230e0e85
Веретенников Егор <etennikoff@gmail.com>
2026-01-04 12:48:18 +0500

    Configure admin panel with list_display and search_fields

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import ServiceType, Apartment, Tariff, PaymentRecord
 
-# Register your models here.
+
+@admin.register(ServiceType)
+class ServiceTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'unit', 'norm_per_person')
+    search_fields = ('name',)
+
+
+@admin.register(Apartment)
+class ApartmentAdmin(admin.ModelAdmin):
+    list_display = ('address', 'user', 'area', 'residents_count', 'created_at')
+    search_fields = ('address', 'user__username')
+    list_filter = ('created_at',)
+
+
+@admin.register(Tariff)
+class TariffAdmin(admin.ModelAdmin):
+    list_display = ('service_type', 'region', 'price', 'valid_from', 'valid_to')
+    search_fields = ('region', 'service_type__name')
+    list_filter = ('service_type', 'region')
+
+
+@admin.register(PaymentRecord)
+class PaymentRecordAdmin(admin.ModelAdmin):
+    list_display = ('apartment', 'service_type', 'date', 'consumption', 'total_amount', 'is_overpayment')
+    search_fields = ('apartment__address',)
+    list_filter = ('service_type', 'is_overpayment', 'date')
+    date_hierarchy = 'date'
