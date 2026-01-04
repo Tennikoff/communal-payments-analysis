0d532e02aff06616fb0011aa4707ffc31fbe7049
Веретенников Егор <etennikoff@gmail.com>
2026-01-04 14:09:39 +0500

    Add base template and home page with Bootstrap

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Apartment, PaymentRecord
 
-# Create your views here.
+
+def home(request):
+    """Главная страница"""
+    stats = {
+        'users_count': User.objects.count(),
+        'apartments_count': Apartment.objects.count(),
+        'payments_count': PaymentRecord.objects.count(),
+    }
+    return render(request, 'home.html', {'stats': stats})
