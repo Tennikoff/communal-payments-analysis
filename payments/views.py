4528de9b94e2f7bee161db8ca001cb25ef46d76f
Веретенников Егор <etennikoff@gmail.com>
2026-01-04 16:34:04 +0500

    Fix logout functionality with POST request

--- payments/views.py
+++ payments/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Apartment, PaymentRecord
+from .forms import RegisterForm
 
 
 def home(request):
@@ -11,3 +14,17 @@ def home(request):
         'payments_count': PaymentRecord.objects.count(),
     }
     return render(request, 'home.html', {'stats': stats})
+
+
+def register(request):
+    """Регистрация нового пользователя"""
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Регистрация успешна! Добро пожаловать!')
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
