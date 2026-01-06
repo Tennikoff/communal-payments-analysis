959de1d4786bf21f7caaa2f3bb94652c3dd6e37a
Веретенников Егор <etennikoff@gmail.com>
2026-01-06 14:32:15 +0500

    configure settings for production deploy

--- config/settings.py
+++ config/settings.py
@@ -116,3 +116,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Авторизация
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+
+# Статика для продакшена
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'Tennikoff.pythonanywhere.com']
